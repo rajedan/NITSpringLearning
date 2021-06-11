c037b66fd91e05845441269455de5e3eb3f5aefc
rajeshkumar.dansena <rajeshkumar.dansena@servicenow.com>
2021-06-11 07:47:38 +0530

    init-class02

---
+++ class02/src/main/java/class02/class02/App.java
@@ -0,0 +1,13 @@
+package class02.class02;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
